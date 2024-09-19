183773fc250a39cce5d23a5307c6bbf1215fee6e
김현승 <gimhyeonseung@gimhyeonseung-ui-MacBookAir.local>
2024-09-19 15:32:02 +0900

    first commit

---
+++ src/lib/jwtUtills.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (user) => {
+  const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, {
+    expiresIn: '1h',
+  });
+  return token;
+};
+
+export const verifyToken = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+  
+    if (!token) {
+      return res.status(401).json({ message: 'Access Denied' });
+    }
+  
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      res.status(400).json({ message: 'Invalid Token' });
+    }
+  };
